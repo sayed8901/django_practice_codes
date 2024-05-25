6c259e41e715d9e5c98875d8edc69b7839ebc3aa
sayed8901 <sayed91515@gmail.com>
2024-05-25 16:36:49 +0600

    loop & condition practice

--- practice_module_10_5/first_app/views.py
+++ practice_module_10_5/first_app/views.py
@@ -5,7 +5,24 @@ def app_home(request):
     data = {
         'age': 20,
         'bio': 'Hello, This is Sayed from Naraynagnaj',
-        'text_list': ['Hello', 'I', 'am', 'Sayed.']
+        'text_list': ['Hello', 'I', 'am', 'Sayed.'],
+        'courses': [
+            {
+                'id': 1,
+                'name': 'Mathematics',
+                'fee': 2000
+            },
+            {
+                'id': 2,
+                'name': 'Physics',
+                'fee': 2500
+            },
+            {
+                'id': 3,
+                'name': 'English',
+                'fee': 3000
+            },
+        ]
     }
     return render(request, 'home.html', context = data)
 
